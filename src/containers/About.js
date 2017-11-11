33a26637cb1a976529ec495ecbd5d8f97212c75b
fubz <the.fubz@gmail.com>
2017-11-11 01:28:19 -0500

    initial site

---
+++ src/containers/About.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+//
+
+export default () => (
+  <div>
+    <h1>This is what we're all about.</h1>
+    <p>React, programming, understanding the universe, and the sense of purpose.</p>
+  </div>
+)
